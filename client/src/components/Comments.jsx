62cd313e2c6455e3238b8a344ddc680212fa1b04
Alex Campo <abrettc@gmail.com>
2022-08-15 13:12:43 -0500

    comments card mapped out with functioning axios calls

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,7 +1,35 @@
-export default function Comments () {
-  return (
-    <nav>
-      <h1>I am nav bar</h1>
-    </nav>
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { BASE_URL } from "../services/api"
+
+export default function Comments (props) {
+  let [currentComment, setCurrentComment] = useState(null)
+
+  const getComment = async () => {
+    let res = await axios.get(`${BASE_URL}/comment/${props.comment.id}`)
+    console.log(res.data)
+    setCurrentComment(res.data)
+  }
+
+  useEffect(() => {
+    getComment()
+  }, [])
+
+
+  return currentComment ? (
+    <div>
+      <div className='comment-header'>
+        <img src={currentComment.User.profileImg} alt='profilepic' style={{height: '30px', width: '30px'}}></img>
+        <h3>{currentComment.User.username}</h3>
+      </div>
+      <div className="comment-body">
+        <p>{currentComment.comment}</p>
+        <p>{currentComment.numLikes} likes</p>
+      </div>
+    </div>
+  ) : (
+    <div>
+      <h3>Loading...</h3>
+    </div>
   )
   }
