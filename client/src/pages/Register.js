fb050644705b5ff7b4b45792231c7f48f1c410fa
Your Actual Name Here <SHA256:hOXSm53Eq3p5Z4sDLk3h/LjyDO3EhgoJf0cCmjpAP04>
2022-08-15 15:19:52 -0400

    edits to sign in and register

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,17 @@
-export default function Register() {
+export default function Register(props) {
+  const newProfile = props.user;
   return (
-    <div className="login">
-      <h1>Login</h1>
+    <div className="register">
+      <h1>Register</h1>
       <form>
         <label htmlFor="username">Username:</label>
         <input id="username" type="text" />
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="text" />
         <label htmlFor="password">Password:</label>
         <input id="password" type="password" />
+        {/* <label htmlFor="profilePicture">Profile Picture:</label>
+        <input type="text" value={newProfile.profilePicture} /> */}
         <button type="submit">Login</button>
       </form>
     </div>
