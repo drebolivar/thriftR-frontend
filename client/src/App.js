66fa85140d57369991d0e981f9fe5712e3a2f2e1
Alex Campo <abrettc@gmail.com>
2022-08-16 09:25:28 -0500

    added create post component and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Profile from './pages/Profile'
 import Register from './pages/Register'
 import SignIn from './pages/SignIn'
 import Client from './services/api'
+import CreatePost from './components/CreatePost'
 import { BASE_URL } from './services/api'
 import axios from 'axios'
 import { CheckSession } from './services/Auth'
@@ -15,14 +16,13 @@ import { useNavigate } from 'react-router-dom'
 
 function App() {
   let navigate = useNavigate()
-  
+
   const [authenticated, toggleAuthenticated] = useState(false)
   const [user, setUser] = useState(null)
-  const [signedIn, setSignIn] = useState(true)
+  const [signedIn, setSignIn] = useState(false)
   const [allPosts, setAllPosts] = useState(null)
   const [allUserPosts, setAllUserPosts] = useState([])
-  
- 
+
   const getUser = async () => {
     const res = await axios.get(`${BASE_URL}/users/1`)
     console.log(res.data)
@@ -35,25 +35,24 @@ function App() {
     setAllPosts(res.data)
   }
 
-
   const handleLogOut = () => {
     //Reset all auth related state and clear localStorage
     setUser(null)
     toggleAuthenticated(false)
     localStorage.clear()
   }
-  
+
   const checkToken = async () => {
     const user = await CheckSession()
     setUser(user)
     toggleAuthenticated(true)
   }
-  
+
   const getUserPosts = async () => {
     const res = await axios.get(`${BASE_URL}/feed/profile/1`)
     setAllUserPosts(res.data)
   }
-  
+
   useEffect(() => {
     const token = localStorage.getItem('token')
     if (token) {
@@ -66,7 +65,7 @@ function App() {
 
   return (
     <div className="App">
-      <Nav signedIn={signedIn} userId={user.id} />
+      <Nav signedIn={signedIn} />
       <img src={logo} alt="logo" />
       <Routes>
         <Route path="/" element={<Feed posts={allPosts} />} />
@@ -76,13 +75,17 @@ function App() {
             <SignIn
               setUser={setUser}
               toggleAuthenticated={toggleAuthenticated}
+              setSignIn={setSignIn}
             />
           }
         />
         <Route path="/register" element={<Register />} />
+        <Route path="/newpost" element={<CreatePost />} />
         <Route
           path="/feed"
-          element={<Feed user={user} posts={allPosts} authenticated={authenticated} />}
+          element={
+            <Feed user={user} posts={allPosts} authenticated={authenticated} />
+          }
         />
         <Route path="/profile/:id" element={<Profile posts={allUserPosts} />} />
       </Routes>
@@ -91,4 +94,3 @@ function App() {
 }
 
 export default App
-
